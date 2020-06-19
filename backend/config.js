8feeb4e92bbc039c4f95f5c78fad616daf2d2f5f
Yassineaeh <34603692+Yassineaeh@users.noreply.github.com>
2020-06-19 21:17:04 +0000

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export default {
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/docpharma',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'Aesj8gdhOIWNXRPLP6VnNm-sv2SSKv5N-dGcrQl8XGe3D1r3ktphsLanm_raa_boKLi5rfx9JGFJgq9b'
+
+}
